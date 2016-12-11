1d92c5625086546cc860f2be758a1702540bf30d
ammanin <ammanin@andrew.cmu.edu>
2016-12-11 10:05:51 -0500

    tryy

--- app.rb
+++ app.rb
@@ -129,9 +129,9 @@ class CustomHandler < AlexaSkillsRuby::Handler
 	language = (request.intent.slots["language"] )
     translate_url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + "en" + "&tl=" + "fr" + "&dt=t&q=" + URI.escape(translation)
   
-	response = HTTParty.get translate_url
-    puts response.to_s
-    response.to_s
+	#response = HTTParty.get translate_url
+    #puts response.to_s
+    #response.to_s
 	response.set_output_speech_text("okay")  
 	
 	
