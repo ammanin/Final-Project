7eec2b95328856e563120f76d150f8ac6546b3fb
ammanin <ammanin@andrew.cmu.edu>
2016-12-11 23:44:00 -0500

    d

--- app.rb
+++ app.rb
@@ -121,10 +121,10 @@ class CustomHandler < AlexaSkillsRuby::Handler
   on_intent("Translate") do
     slots = request.intent.slots
     puts slots.to_s
-    translation_txt = (request.intent.slots["translation"] )
-	language_input = (request.intent.slots["language"] )
+    translation = (request.intent.slots["translation"] )
+	language = (request.intent.slots["language"] )
 	
-	response.set_output_speech_text("You want me to translate #{translation_txt} to #{langauge_input}" )  
+	response.set_output_speech_text("You want me to translate #{translation} to #{langauge}" )  
     #response.set_simple_card("title", "content")
   end
 
@@ -150,7 +150,6 @@ post '/' do
 end
 
 get '/' do
-   square_of "hello there"
 end
 # THE APPLICATION ID CAN BE FOUND IN THE 
 
